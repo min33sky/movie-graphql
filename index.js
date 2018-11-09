ad1bbf2ba5f8c65f83fc23695bff7db9b3599850
min33sky <min33sky@naver.com>
2018-11-09 20:15:57 +0900

    heroku go

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import resolvers from './graphql/resolvers';
  */
 const server = new GraphQLServer({
   typeDefs: 'graphql/schema.graphql',
-  resolvers,
+  resolvers
 });
 
 server.start(() => console.log('Graphql Server Running'));
