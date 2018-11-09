839cc78d2080e62891b21ee07122a091fb06e9a6
min33sky <min33sky@naver.com>
2018-11-09 20:45:51 +0900

    Updates

---
+++ precache-manifest.4e2b6153e8bccfe729eb66bb002031cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b643fd8c6fda99e7b36",
+    "url": "/movie-graphql/static/js/runtime~main.2b643fd8.js"
+  },
+  {
+    "revision": "e7b5237d8df6ee40056d",
+    "url": "/movie-graphql/static/js/main.e7b5237d.chunk.js"
+  },
+  {
+    "revision": "b4acefde87ee39db1041",
+    "url": "/movie-graphql/static/js/1.b4acefde.chunk.js"
+  },
+  {
+    "revision": "a27e0ecbdf2f3b2b0d9ee0bfb8d56a41",
+    "url": "/movie-graphql/index.html"
+  }
+];
